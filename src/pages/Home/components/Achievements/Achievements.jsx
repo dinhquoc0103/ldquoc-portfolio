84b7a8a8e2718ede66162370823e9de968c3a708
quoc le <dinhquoc0103@gmail.com>
2023-04-14 15:38:26 +0700

    responsive achievements

--- src/pages/Home/components/Achievements/Achievements.jsx
+++ src/pages/Home/components/Achievements/Achievements.jsx
@@ -14,22 +14,22 @@ function Achievements() {
                 <p className="mt-3 text-lg text-gray-400">My achievements while studying at school</p>
             </div>
 
-            <div className="mt-24 py-5 px-8 bg-gray-700 max-w-4xl mx-auto rounded-2xl flex items-center gap-6 relative min-h-[24rem]">
-                <div className="max-w-lg">
-                    <h2 className="text-2xl font-semibold">Some academic achievements</h2>
+            <div className="mt-24 py-5 px-8 bg-gray-700 max-w-4xl mx-auto rounded-2xl flex flex-col-reverse lg:flex-row items-center gap-6 relative min-h-[24rem]">
+                <div className="max-w-lg ">
+                    <h2 className="text-2xl font-semibold text-center md:text-left">Some academic achievements</h2>
 
-                    <p className="text-sm mt-4 text-left leading-6">
+                    <p className="text-sm mt-4 text-justify md:text-left leading-6">
                         Very Good Certificate of Merit for Semester I, II (2018 - 2019), Semester I (2019 - 2020), Semester II (2020 - 2021). Scholarships to encourage study, very good grades for semesters I and II (2019-2020).
                     </p>
 
-                    <a href="#" className="mt-10 block">
+                    <a href="#" className="mt-10 block text-center md:text-left">
                         <Button>
-                            Go to my achievements
+                            See It
                         </Button>
                     </a>
                 </div>
 
-                <div className="absolute h-[32rem] bottom-0 -right-3 ">
+                <div className="lg:absolute h-80 lg:h-[32rem] lg:bottom-0 lg:-right-4">
                     <img src={ACHIEVEMENTS_IMAGE} alt="My Achievements" className="w-full h-full object-cover" />
                 </div>
             </div>
